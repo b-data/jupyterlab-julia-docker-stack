392db195b337d8896a5be1b46b00be265e77c8cd
Olivier Benz <olivier.benz@b-data.ch>
2020-03-29 12:17:43 +0200

    Update Code Server to version 3.0.1
    - aka VS Code version 1.43.0
    - Update extenstion Julia to version 0.15.18
    - Pin jupyter-server-proxy to version 1.2.0

--- ver/jupyter_notebook_config.py
+++ ver/jupyter_notebook_config.py
@@ -12,7 +12,6 @@ c = get_config()
 c.KernelSpecManager.whitelist=["julia-1.4"]
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
