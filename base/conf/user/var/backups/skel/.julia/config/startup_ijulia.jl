54e502c440b1ee3f94180efc1b5a96cb1839b407
Olivier Benz <olivier.benz@b-data.ch>
2023-05-07 13:16:49 +0200

    IJulia: Includes user's private bin in PATH

--- base/conf/user/var/backups/skel/.julia/config/startup_ijulia.jl
+++ base/conf/user/var/backups/skel/.julia/config/startup_ijulia.jl
@@ -1,3 +1,17 @@
 if !in(MIME("application/pdf"), IJulia.ijulia_mime_types)
     IJulia.register_mime(MIME("application/pdf"))
 end
+
+if ispath(joinpath(ENV["HOME"], "bin")) && ENV["SHLVL"] == "0"
+    ENV["PATH"] =
+        joinpath(ENV["HOME"], "bin") *
+        (Sys.iswindows() ? ';' : ':') *
+        ENV["PATH"]
+end
+
+if ispath(joinpath(ENV["HOME"], ".local/bin")) && ENV["SHLVL"] == "0"
+    ENV["PATH"] =
+        joinpath(ENV["HOME"], ".local/bin") *
+        (Sys.iswindows() ? ';' : ':') *
+        ENV["PATH"]
+end
