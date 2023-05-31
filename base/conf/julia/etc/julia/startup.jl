88b4f8c4d262c1ecd3482400ede0f118ac456645
Olivier Benz <olivier.benz@b-data.ch>
2023-05-31 15:42:16 +0200

    Update startup files

--- base/conf/julia/etc/julia/startup.jl
+++ base/conf/julia/etc/julia/startup.jl
@@ -1,8 +1,9 @@
-# This file should contain site-specific commands to be executed on Julia startup;
+# This file contains site-specific commands to be executed on Julia startup;
 # Users may store their own personal commands in `~/.julia/config/startup.jl`.
 
 println("Executing site-specific startup file (", @__FILE__, ")...")
 
-push!(LOAD_PATH, "$(ENV["JULIA_PATH"])/local/share/julia/environments/v$(VERSION.major).$(VERSION.minor)")
+push!(LOAD_PATH, joinpath("$(ENV["JULIA_PATH"])", "local", "share", "julia",
+        "environments", "v$(VERSION.major).$(VERSION.minor)"))
 
 using Pkg
--- base/conf/user/var/backups/skel/.julia/config/startup.jl
+++ base/conf/user/var/backups/skel/.julia/config/startup.jl
@@ -1,5 +1,7 @@
 println("Executing user-specific startup file (", @__FILE__, ")...")
 
+Pkg.activate(; io=devnull)
+
 try
     using Revise
     println("Revise started")
@@ -7,10 +9,13 @@ catch e
     @warn "Error initializing Revise" exception=(e, catch_backtrace())
 end
 
-if isfile(joinpath(pwd(), "Project.toml")) && isfile(joinpath(pwd(), "Manifest.toml"))
+if isfile(joinpath(pwd(), "Project.toml")) &&
+    isfile(joinpath(pwd(), "Manifest.toml"))
+
     Pkg.activate(pwd())
 else
-    Pkg.activate("$(ENV["HOME"])/.julia/environments/v$(VERSION.major).$(VERSION.minor)")
+    Pkg.activate(joinpath("$(ENV["HOME"])", ".julia", "environments",
+            "v$(VERSION.major).$(VERSION.minor)"))
 end
 if "TERM_PROGRAM" in keys(ENV)
     println("  → Overrides the environment set by the Code Julia extension!")
