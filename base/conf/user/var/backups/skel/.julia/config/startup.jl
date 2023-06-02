294d8b82c3bbbaf283a17cab3d0aff9cccd01fd8
Olivier Benz <olivier.benz@b-data.ch>
2023-06-02 15:56:23 +0200

    Update user-specific startup file

--- base/conf/user/var/backups/skel/.julia/config/startup.jl
+++ base/conf/user/var/backups/skel/.julia/config/startup.jl
@@ -1,6 +1,12 @@
 println("Executing user-specific startup file (", @__FILE__, ")...")
 
-Pkg.activate(; io=devnull)
+# https://github.com/julia-vscode/julia-vscode/issues/3304
+project = (Base.JLOptions().project != C_NULL ?
+    unsafe_string(Base.JLOptions().project) :
+    get(ENV, "JULIA_PROJECT", nothing))
+if !isnothing(project)
+    Pkg.activate(; io=devnull)
+end
 
 try
     using Revise
@@ -9,14 +15,31 @@ catch e
     @warn "Error initializing Revise" exception=(e, catch_backtrace())
 end
 
-if isfile(joinpath(pwd(), "Project.toml")) &&
-    isfile(joinpath(pwd(), "Manifest.toml"))
+if !isnothing(project) &&
+    # https://github.com/julia-vscode/julia-vscode/issues/3304
+    !startswith(Base.load_path_expand(Base.LOAD_PATH[end]), project)
 
-    Pkg.activate(pwd())
+    if startswith(project, "@")
+        if startswith(project, "@.")
+            if isnothing(Base.current_project())
+                Pkg.activate(joinpath("$(ENV["HOME"])", ".julia",
+                        "environments", "v$(VERSION.major).$(VERSION.minor)"))
+            else
+                Pkg.activate(Base.current_project(); io=devnull)
+            end
+        else
+            Pkg.activate(Base.load_path_expand(project); io=devnull)
+        end
+    else
+        Pkg.activate(abspath(expanduser(project)); io=devnull)
+    end
 else
-    Pkg.activate(joinpath("$(ENV["HOME"])", ".julia", "environments",
-            "v$(VERSION.major).$(VERSION.minor)"))
-end
-if "TERM_PROGRAM" in keys(ENV)
-    println("  → Overrides the environment set by the Code Julia extension!")
+    if isfile(joinpath(pwd(), "Project.toml")) &&
+        isfile(joinpath(pwd(), "Manifest.toml"))
+
+        Pkg.activate(pwd())
+    else
+        Pkg.activate(joinpath("$(ENV["HOME"])", ".julia", "environments",
+                "v$(VERSION.major).$(VERSION.minor)"))
+    end
 end
