7a7bfdcdd3f2df17f77efcefef4278fcc5a0d8a5
Olivier Benz <olivier.benz@b-data.ch>
2023-06-04 20:49:15 +0200

    Keep header of original site-specific startup file

--- base/conf/julia/etc/julia/startup.jl
+++ base/conf/julia/etc/julia/startup.jl
@@ -1,4 +1,4 @@
-# This file contains site-specific commands to be executed on Julia startup;
+# This file should contain site-specific commands to be executed on Julia startup;
 # Users may store their own personal commands in `~/.julia/config/startup.jl`.
 
 println("Executing site-specific startup file (", @__FILE__, ")...")
