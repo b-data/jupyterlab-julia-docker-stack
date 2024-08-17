10934eaa8dfd52cdb26496c47973d8414b56bda3
Olivier Benz <olivier.benz@b-data.ch>
2024-08-17 12:41:57 +0200

    Update script 10-env.sh

--- base/scripts/usr/local/bin/before-notebook.d/10-env.sh
+++ base/scripts/usr/local/bin/before-notebook.d/10-env.sh
@@ -8,15 +8,11 @@ set -e
 LANG=${LANG:=en_US.UTF-8}
 TZ=${TZ:=Etc/UTC}
 
-if [ "$(id -u)" == 0 ] ; then
-  # Update timezone if needed
-  if [ "$TZ" != "Etc/UTC" ]; then
-    _log "Setting TZ to $TZ"
-    ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime \
-      && echo "$TZ" > /etc/timezone
-  fi
+# Info about timezone
+_log "TZ is set to $TZ (/etc/localtime and /etc/timezone remain unchanged)"
 
-  # Add/Update locale if needed
+if [ "$(id -u)" == 0 ] ; then
+  # Add/Update locale if requested
   if [ -n "$LANGS" ]; then
     for i in $LANGS; do
       sed -i "s/# $i/$i/g" /etc/locale.gen
@@ -29,25 +25,21 @@ if [ "$(id -u)" == 0 ] ; then
     locale-gen --keep-existing
   fi
   update-locale --reset LANG="$LANG"
-  _log "INFO: LANG is set to $LANG"
+  # Info about locale
+  _log "LANG is set to $LANG"
 else
-  # Warn if the user wants to change the timezone but hasn't started the
-  # container as root.
-  if [ "$TZ" != "Etc/UTC" ]; then
-    _log "WARNING: Setting TZ to $TZ but /etc/localtime and /etc/timezone remain unchanged!"
-  fi
-
   # Warn if the user wants to add locales but hasn't started the container as
   # root.
   if [ -n "$LANGS" ]; then
     _log "WARNING: Container must be started as root to add locale(s)!"
   fi
-  # Warn if the user wants to change to a local that is not available.
+  # Warn if the user wants to change to a locale that is not available.
   if ! grep -v '^#' < /etc/locale.gen | grep -q "$LANG"; then
     _log "WARNING: Locale $LANG is not available!"
     . /etc/default/locale
     _log "WARNING: Resetting LANG to $LANG"
   else
-    _log "INFO: LANG is set to $LANG"
+    # Info about locale
+    _log "LANG is set to $LANG"
   fi
 fi
